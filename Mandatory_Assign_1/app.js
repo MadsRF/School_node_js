6136a0bfe580c7ff8bbba16fb8d55386553b1d4c
MadsRF <madsrunefrederiksen@gmail.com>
2020-03-04 23:50:24 +0100

    04-mar-20

--- Mandatory_Assign_1/app.js
+++ Mandatory_Assign_1/app.js
@@ -1,13 +1,31 @@
 const express = require("express");
 const app = express();
 const PORT = 8000;
+const publicDir = require('path').join(__dirname,'/public');
+app.use(express.static(publicDir));
 
 
 app.get(("/"), (req, res) => {
     console.log("index");
-
     return res.sendFile(__dirname + "/public/index.html");
 });
+
+app.get(("/script.js"), (req, res) => {
+    console.log("script.js");
+    return res.sendFile(__dirname + "/public/script.js");
+});
+
+app.get(("/css.css"), (req, res) => {
+    console.log("css");
+    return res.sendFile(__dirname + "/public/css.css");
+});
+
+app.get(("/json_page"), (req, res) => {
+    console.log("json_page");
+    return res.sendFile(__dirname + "/public/json_page.html");
+});
+
+
 
 
 
---
+++ Mandatory_Assign_1/public/script.js
@@ -0,0 +1,23 @@
+$(document).ready( () => {
+
+    $("#go_to_main_menu").click( () => {
+        //alert("test")
+        window.location.replace("/");
+    });
+
+    $("#go_to_json_page").click( () => {
+        //alert("test 1")
+        window.location.replace("/json_page");
+    });
+
+
+    
+});
+
+
+$(document).ready( () =>{
+
+    $(".made_by_footer").html("<p>Mads Rune Frederiksen</p>");
+
+
+});
