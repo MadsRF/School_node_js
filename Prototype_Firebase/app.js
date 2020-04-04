d3454a7c71340054cf393382316778917ff5a22f
MadsRF <madsrunefrederiksen@gmail.com>
2020-04-04 19:26:12 +0200

    04/04-20

--- Prototype_Firebase/app.js
+++ Prototype_Firebase/app.js
@@ -17,27 +17,29 @@ let db = admin.firestore();
 // Firebase Collection
 let docRef = db.collection('coffee').doc();
 
-// data to send 
+// sends data to the firebase database  
 //let setCoffee = docRef.set({ title: "test fra node" });
 
-// 
+// http get request
 app.get("/", (req, res) => {
     console.log("frontpage");
 
+    // For loop goes thorugh our collection list and displays them in log. 
     db.collection('coffee').get().then((snapshot) => {
         snapshot.forEach((doc) => {
           console.log(doc.id, '=>', doc.data().title);
         });
       })
+      // Error handling for not being able to get data
       .catch((err) => {
         console.log('Error getting documents', err);
       });
 
-
-
+    // Returns frontpage.html when accessing localhost:9000/
     return res.sendFile(__dirname + "/public/frontpage.html");
 });
 
+// listening for incoming trafic on port 9000
 app.listen(9000);
 
 
